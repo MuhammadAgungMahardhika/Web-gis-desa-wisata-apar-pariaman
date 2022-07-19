f9f3380e1e3182b83d1bad6927fce52c4b2b159a
MuhammadAgungMahardhika <m.agungmahardika12@gmail.com>
2022-07-19 19:43:44 +0700

    k

---
+++ app/Database/Seeds/GalleryAtractionSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class GalleryAtractionSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'id' => 'A09',
+            'name'    => 'tesSeeder',
+            'status'    => 3,
+            'price'    => 200,
+            'contact_person'    => 6281373517899,
+            'description'    => 'okee',
+            'lat'    => '-0.601102',
+            'lng'    => '100.108668',
+            // 'geom'   => 'ST_PolygonFromText("POLYGON((0 0,10 0,10 10,0 10,0 0),(5 5,7 5,7 7,5 7, 5 5))'
+
+        ];
+
+        // Simple Queries
+        // $this->db->query('INSERT INTO users (username, email) VALUES(:username:, :email:)', $data);
+
+        // Using Query Builder
+        $this->db->table('atraction')->insert($data);
+    }
+}
