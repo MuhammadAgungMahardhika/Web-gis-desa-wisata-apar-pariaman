752f225608dfb45198887c1edf526a806d61ea3a
MuhammadAgungMahardhika <m.agungmahardika12@gmail.com>
2022-08-09 19:42:00 +0700

    k

--- app/Controllers/ReviewController.php
+++ app/Controllers/ReviewController.php
@@ -20,8 +20,6 @@ class ReviewController extends BaseController
             $data = $this->request->getPOST();
             $user_id = $this->request->getPOST('user_id');
             $atraction_id = $this->request->getPOST('atraction_id');
-            $ratingValue = $this->request->getPOST('rating');
-            // $event_id = $this->request->getPOST('event_id');
 
             if ($user_id && $atraction_id) {
                 $check = $this->modelReview->check($user_id, $atraction_id)->getRow();
@@ -30,6 +28,33 @@ class ReviewController extends BaseController
                     date_default_timezone_set('Asia/Jakarta');
                     $data['updated_date'] =  date('Y-m-d H:i:s');
                     $updateRating = $this->modelReview->updateAtractionRating($data, $user_id, $atraction_id);
+                    if ($updateRating == true) {
+                        return json_encode($updateRating);
+                    }
+                } else {
+                    $addRating = $this->modelReview->addRating($data);
+                    if ($addRating == true) {
+                        return json_encode($addRating);
+                    }
+                }
+            }
+        }
+    }
+    public function event()
+    {
+        //untuk ajax
+        if ($this->request->isAJAX()) {
+            $data = $this->request->getPOST();
+            $user_id = $this->request->getPOST('user_id');
+            $event_id = $this->request->getPOST('event_id');
+
+            if ($user_id && $event_id) {
+                $check = $this->modelReview->check($user_id, $event_id)->getRow();
+                // check rating alredy exist or not // if exist update // if not insert
+                if ($check->rating != null) {
+                    date_default_timezone_set('Asia/Jakarta');
+                    $data['updated_date'] =  date('Y-m-d H:i:s');
+                    $updateRating = $this->modelReview->updateAtractionRating($data, $user_id, $event_id);
                     if ($updateRating == true) {
                         return json_encode($updateRating);
                     }
