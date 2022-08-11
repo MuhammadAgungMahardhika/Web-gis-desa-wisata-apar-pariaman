9ea88c7399fb704a967eff1868024aabb97663c1
MuhammadAgungMahardhika <m.agungmahardika12@gmail.com>
2022-08-11 15:59:53 +0700

    k

--- app/Views/layout/sidebar_detail.php
+++ app/Views/layout/sidebar_detail.php
@@ -1,7 +1,7 @@
 <div class="d-flex justify-content-center avatar avatar-xl me-3" id="avatar-sidebar">
     <img src="<?= base_url('/assets/images/pesona_apar.png'); ?>" alt="" srcset="">
 </div>
-<?php if (in_groups('users') || in_groups('admin')) : ?>
+<?php if (in_groups('user') || in_groups('admin')) : ?>
     <div class="p-2 d-flex justify-content-center">Hello, <?= user()->username; ?></div>
 <?php else : ?>
     <div class="p-2 d-flex justify-content-center">Hello, Visitor</div>
