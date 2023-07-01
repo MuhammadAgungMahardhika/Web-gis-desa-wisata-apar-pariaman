45857cb5b32edbd9f975bb9459e60d185e5df4ea
Muhammad Agung Mahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2023-07-01 18:56:31 +0700

    Delete vendor directory

--- vendor/myth/auth/src/Exceptions/PermissionException.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Myth\Auth\Exceptions;
-
-class PermissionException extends \RuntimeException implements ExceptionInterface
-{
-}
--- vendor/myth/auth/src/Exceptions/UserNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Myth\Auth\Exceptions;
-
-class UserNotFoundException extends \RuntimeException implements ExceptionInterface
-{
-    public static function forUserID(int $id)
-    {
-        return new self(lang('Auth.userNotFound', [$id]), 404);
-    }
-}
